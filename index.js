6b754e5478ddc851408f1e6e97b25e3720cfa94b
vinh <vqt123@gmail.com>
2023-12-17 03:41:39 -0500

    remove typescript def

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 import data from './data.js';
 
+function generateRandomWomanProfile() {
+    const race = data.races[Math.floor(Math.random() * data.races.length)];
+    const contries = data.raceToCountries[race];
+    const country = contries[Math.floor(Math.random() * contries.length)];    
+    const gender = 'woman';
+    const hair_colors = data.raceToHairColors[race];
+    const hair_color = hair_colors[Math.floor(Math.random() * hair_colors.length)]
+    const first_name = (gender == 'woman') ? data.countryToFemaleNames[country][Math.floor(Math.random() * data.countryToFemaleNames[country].length)] : data.countryToMaleNames[country][Math.floor(Math.random() * data.countryToMaleNames[country].length)];
+    const last_name = data.countryToLastNames[country][Math.floor(Math.random() * data.countryToLastNames[country].length)];
+
+
+    return { first_name, last_name, race, country, gender, hair_color};
+}
+
+function generateRandomManProfile() {
+    const race = data.races[Math.floor(Math.random() * data.races.length)];
+    const contries = data.raceToCountries[race];
+    const country = contries[Math.floor(Math.random() * contries.length)];    
+    const gender = 'man';
+    const hair_colors = data.raceToHairColors[race];
+    const hair_color = hair_colors[Math.floor(Math.random() * hair_colors.length)]
+    const first_name = (gender == 'woman') ? data.countryToFemaleNames[country][Math.floor(Math.random() * data.countryToFemaleNames[country].length)] : data.countryToMaleNames[country][Math.floor(Math.random() * data.countryToMaleNames[country].length)];
+    const last_name = data.countryToLastNames[country][Math.floor(Math.random() * data.countryToLastNames[country].length)];
+
+
+    return { first_name, last_name, race, country, gender, hair_color};
+}
+
 
 function generateRandomProfile() {
     const race = data.races[Math.floor(Math.random() * data.races.length)];
@@ -16,4 +44,4 @@ function generateRandomProfile() {
 }
 
 
-export default generateRandomProfile;
+export default {generateRandomProfile, generateRandomWomanProfile, generateRandomManProfile};
